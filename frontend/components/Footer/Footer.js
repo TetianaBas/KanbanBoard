107b96dd66be49166de1d4c45e3186534b1b05d9
Tetiana Bas <91345827+TetianaBas@users.noreply.github.com>
2024-10-07 23:05:25 +0100

    Create Footer.js

---
+++ frontend/components/Footer/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './Footer.module.css'; // Import the CSS module
+
+const Footer = () => (
+    <footer className={styles.footer}>
+        &copy; 2024 Minerva University CS162
+    </footer>
+);
+
+export default Footer;
